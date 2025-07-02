97fdc3675be267dfef59f19b3f5a089bd87f42d7
cnunezjacas <cnunezjacas@gmail.com>
2025-07-02 13:36:17 -0400

    verifica si una cuenta de banco se encuentra en el sistema

---
+++ src/utils/utils_axios/entity/verificarCuentaDeBanco.js
@@ -0,0 +1,23 @@
+import { STRINGS } from 'utils/string.js'
+import api from 'src/axios.js'
+
+/* Función para verificar si el código ya existe */
+const verificarCuentaDeBanco = async (codigo) => {
+  var aux = false
+  try {
+    const response = await api.get(`${STRINGS.urlApiCuenta}?/numero=${codigo}`)
+    // Asumiendo que tu API devuelve un array de resultados
+    response.data.forEach((element) => {
+      if (element.numero === codigo) {
+        aux = true
+      }
+    })
+    return aux ? true : false
+  } catch (error) {
+    console.error('Error al verificar código:', error)
+    // Si hay un error en la consulta, asumimos que no hay duplicado para no bloquear
+    return false
+  }
+}
+
+export default verificarCuentaDeBanco
