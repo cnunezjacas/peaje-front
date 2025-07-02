ca4b1ac875d5975325f648edcdbb7c4c92240d25
cnunezjacas <cnunezjacas@gmail.com>
2025-07-02 13:44:00 -0400

    cambios en la forma de que vincula de manera dinámica las vistas de Nomenclador y Entidad

--- src/pages/manage/gest_global.vue
+++ src/pages/manage/gest_global.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page>
     <div>
-      <div id="boxNom" class="col-12 d-none">
+      <div id="boxNom" ref="RefBoxNom" class="col-12 d-none">
         <tabs_nomencladores ref="RefNom" />
       </div>
     </div>
 
     <div>
-      <div id="boxEnt" class="col-12 d-none">
+      <div id="boxEnt" ref="RefBoxEnt" class="col-12 d-none">
         <tabs_entity />
       </div>
     </div>
@@ -15,7 +15,7 @@
 </template>
 
 <script setup>
-import { onBeforeUpdate, ref } from 'vue'
+import { onBeforeUpdate, ref, nextTick } from 'vue'
 
 import tabs_nomencladores from 'viewsManage/tabs_generic/tabs_nomencladores.vue'
 import tabs_entity from 'viewsManage/tabs_generic/tabs_entity.vue'
@@ -23,17 +23,8 @@ import { STRINGS } from 'src/utils/string'
 
 //Const REF
 const RefNom = ref(null)
-
-// Ejemplo de uso:
-const idBuscado = ref('')
-const objeto = ref({})
-
-const CheckItemFather = async (item) => {
-  //console.log('CheckItemFather:' + JSON.stringify(item))
-  if (item && item.children) {
-    objeto.value = item
-  }
-}
+const RefBoxNom = ref(null)
+const RefBoxEnt = ref(null)
 
 // Cuando quieras llamar a la función:
 const callEmptySelectedTwo = () => {
@@ -45,62 +36,51 @@ const callEmptySelectedTwo = () => {
   }
 }
 
-/*onMounted(() => {
-  alert('onMounted')
-})*/
+const isActiveTabsNomenclator = ref(null)
+const isActiveTabsEntity = ref(null)
 
 onBeforeUpdate(() => {
   callEmptySelectedTwo()
 })
 
+const objeto = ref({})
+
 const CheckItemChildren = async (item) => {
-  console.log('RefNom')
-  console.log(RefNom)
-  idBuscado.value = item[1]
-  if (objeto.value && objeto.value.children) {
+  await nextTick()
+  objeto.value = item
+
+  if (item.length !== 0) {
     // const aux = tieneHijoConId(objeto.value, idBuscado.value)
-    var elementoOne, elementTwo
-    switch (objeto.value.id) {
+    isActiveTabsNomenclator.value = document.getElementById('boxNom')
+    isActiveTabsEntity.value = document.getElementById('boxEnt')
+
+    // Verificar que existan
+    if (isActiveTabsNomenclator.value === null || isActiveTabsEntity.value === null) {
+      console.warn('Elementos DOM no disponibles en este momento')
+    }
+
+    switch (item.idPadre) {
       case STRINGS.gestionNomencladoresLowercase:
-        elementoOne = document.getElementById('boxNom')
-        elementTwo = document.getElementById('boxEnt')
-
-        elementTwo.classList.add('d-none')
-        elementoOne.classList.remove('d-none')
-
+        if (isActiveTabsNomenclator.value) {
+          isActiveTabsNomenclator.value.classList.remove('d-none')
+        }
+        if (isActiveTabsEntity.value) {
+          isActiveTabsEntity.value.classList.add('d-none')
+        }
         break
-
       case STRINGS.gestionEntidadLowercase:
-        elementoOne = document.getElementById('boxNom')
-        elementTwo = document.getElementById('boxEnt')
-
-        elementoOne.classList.add('d-none')
-        elementTwo.classList.remove('d-none')
-
+        if (isActiveTabsNomenclator.value) {
+          isActiveTabsNomenclator.value.classList.add('d-none')
+        }
+        if (isActiveTabsEntity.value) {
+          isActiveTabsEntity.value.classList.remove('d-none')
+        }
         break
     }
   }
 }
 
-// Función para verificar si algún hijo tiene un id específico
-/*const tieneHijoConId = (objeto, idBuscado) => {
-  let aux = false
-  if (!objeto.children || !Array.isArray(objeto.children)) {
-    return false // No tiene hijos
-  }
-
-  //console.log(idBuscado)
-  objeto.children.forEach((element) => {
-    if (element.id === idBuscado) {
-      aux = true
-    }
-  })
-
-  return aux ? aux : false
-}*/
-
 defineExpose({
-  CheckItemFather,
   CheckItemChildren,
 })
 </script>
