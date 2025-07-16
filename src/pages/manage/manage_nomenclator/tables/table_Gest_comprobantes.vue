bf804479b315044da02c31e4bbee29cb9383ba4f
cnunezjacas <cnunezjacas@gmail.com>
2025-07-16 13:46:20 -0400

    refatorización del código en tabla comprobante, exento y moneda, poniendo en uso el componente dinámico tableGeneric

--- src/pages/manage/manage_nomenclator/tables/table_Gest_comprobantes.vue
+++ src/pages/manage/manage_nomenclator/tables/table_Gest_comprobantes.vue
@@ -1,58 +1,43 @@
 <template>
-  <div class="q-px-md">
-    <div class="flex justify-between padding_minimo">
-      <div class="text-center bg-green-10 rounded-borders my-cell">
-        <p class="text-tittle-table">{{ STRINGS.gestion }} {{ STRINGS.comprobanteLowercase }}</p>
-      </div>
-
-      <div>
-        <q-breadcrumbs>
-          <q-breadcrumbs-el class="text-green-10" label="Inicio" to="/" icon="home" />
-          <q-breadcrumbs-el class="text-green-10" :label="STRINGS.gestion" icon="folder" />
-
-          <q-breadcrumbs-el :label="STRINGS.comprobanteLowercase" icon="post_add" />
-        </q-breadcrumbs>
-      </div>
-    </div>
-
-    <!-- Mostrar spinner cuando está cargando -->
-    <div v-if="isLoading" class="flex justify-center items-center" style="height: 300px">
-      <q-spinner size="50px" color="green" />
-    </div>
-    <!-- Mostrar la tabla cuando no está cargando -->
-
-    <q-table
-      v-else
-      class="shadow-2 custom-horizontal-lines"
-      table-header-class="bg-green-10 text-white"
-      ref="tableAddProvincia"
-      :rows-per-page-label="STRINGS.record_page"
-      :rows="filteredRows"
-      :columns="columns"
-      :rows-per-page-options="numberForPage"
-      :no-data-label="STRINGS.no_data_available"
-      row-key="codigo"
-      :separator="separator"
-      selection="single"
-      :selected-rows-label="customSelectedLabel"
-      v-model:selected="selectedRows"
-      @update:selected="onSelectedRowsChange"
-    />
-  </div>
+  <BaseTable
+    :title="title"
+    :Loading="isLoading"
+    :filteredRows="filteredRows"
+    :columns="columns"
+    row-key="codigo"
+    :no-data-label="STRINGS.no_data_available"
+    :separator="separator"
+    :rows-per-page-options="numberForPage"
+    :rows-per-page-label="STRINGS.number_of_page"
+    searchTerm="props.TextSearch"
+    @onEnable="EnableTabs"
+    @seleccionado="DataSelected"
+    ref="tableGeneric"
+  >
+  </BaseTable>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onBeforeMount } from 'vue'
 import { STRINGS } from 'utils/string.js'
 import api from 'src/axios.js'
-import { onBeforeMount } from 'vue'
 import notify_error from 'src/utils/notify/notify_error.js'
 import imports from 'src/utils/imports.js'
+import BaseTable from 'TableManage/tableGeneric.vue'
 
-// Datos
+// ******* Variables *******
+const title = ref(STRINGS.gestion + ' ' + STRINGS.name_comprobantes.toLowerCase())
+const rows = ref([])
+const separator = ref('vertical')
 const numberForPage = imports.getNumberForPage()
+const tableGeneric = ref([])
 const isLoading = ref(true)
+// Props
+const props = defineProps({
+  TextSearch: String,
+})
 
+/* Detalles de las columnas de la tabla, referencias de los nombres de las columnas de la tabla en BD */
 const columns = [
   {
     name: 'codigo',
@@ -89,17 +74,10 @@ const columns = [
   },
 ]
 
-// Función para personalizar la etiqueta del número de filas seleccionadas
-
-const customSelectedLabel = (count) => {
-  return `${count} fila${count > 1 ? 's' : ''} seleccionada${count > 1 ? 's' : ''}`
-}
-
 const InitDataTable = async () => {
   isLoading.value = true
   try {
     const response = await api.get(STRINGS.urlApiComprobante)
-
     const responseMoneda = await imports.getCoin()
 
     response.data.forEach((element) => {
@@ -119,18 +97,6 @@ const InitDataTable = async () => {
   }
 }
 
-onBeforeMount(() => {
-  InitDataTable()
-})
-
-const rows = ref([])
-const separator = ref('vertical')
-const selectedRows = ref([])
-
-const props = defineProps({
-  TextSearch: String,
-})
-
 // Reacción a cambios en TextSearch
 const filteredRows = computed(() => {
   if (!props.TextSearch) {
@@ -146,31 +112,49 @@ const filteredRows = computed(() => {
   })
 })
 
-const emit = defineEmits(['seleccionado'])
-//Manejador de los botones de la navBottom
-var EnableItemsTabs = ref(true)
+const emit = defineEmits(['onEnable'])
 
-// Manejador de selección
-const onSelectedRowsChange = (newSelected) => {
-  if (newSelected.length > 0) {
-    emit('seleccionado', newSelected.length > 0 ? newSelected[0] : null)
-    emit('onEnable', (EnableItemsTabs.value = false))
+/* Función destinada a emit un evento que hablita/desabilita los tabs [Add,Editar,Delete ... ect] */
+const EnableTabs = (value) => {
+  if (value) {
+    emit('onEnable', true)
   } else {
-    emit('onEnable', (EnableItemsTabs.value = true))
+    emit('onEnable', false)
   }
 }
 
+/* Función destinada a recibir de template BaseTable los valores de la fila seleccionada en la tabla */
+const DataSelected = (newSelected) => {
+  if (newSelected.length > 0) {
+    emit('seleccionado', newSelected.length > 0 ? newSelected[0] : null)
+  }
+}
+
+/**
+ * Función encargada actualizar los datos de la tabla
+ */
 const UpdateTable = async () => {
   InitDataTable()
+  tableGeneric.value.selectedRows = []
 }
-
+/**
+ * Función encargada de limpiar los datos capturados en la selección
+ */
 const EmptySelected = () => {
-  selectedRows.value = []
+  tableGeneric.value.selectedRows = []
 }
 
+//******* Métodos *******
+/**
+ * Método encargado de realizar operaciones, previo de que el componente sea montado en el DOM
+ */
+onBeforeMount(() => {
+  InitDataTable()
+})
+
+//Exponer Funciones y Métodos a Template Padre
 defineExpose({
-  filteredRows,
-  EmptySelected,
   UpdateTable,
+  EmptySelected,
 })
 </script>
