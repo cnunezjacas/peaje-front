c5f777358e6e85a40b0e8083ec5845ee9652518d
cnunezjacas <cnunezjacas@gmail.com>
2025-07-28 11:56:50 -0400

    Fichero que contiene las direcciones de todos los elementos del módulo Almacén

---
+++ src/utils/import_files_store.js
@@ -0,0 +1,8 @@
+import { STRINGS } from 'utils/string.js'
+
+//Dialogs Add
+import DialogoAddTarjetaBlanca from 'StoreAdd/dialogAddTarjetaBlanca.vue'
+
+import notify_error from 'utils/notify/notify_error.js'
+
+export { STRINGS, DialogoAddTarjetaBlanca, notify_error }
