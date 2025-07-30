e3e35595d97d388d86e0e821785a172d60e47fbd
cnunezjacas <cnunezjacas@gmail.com>
2025-07-30 09:39:20 -0400

    Importación de fichero dialogoAddProveedor.vue

--- src/utils/import_files_store.js
+++ src/utils/import_files_store.js
@@ -2,7 +2,8 @@ import { STRINGS } from 'utils/string.js'
 
 //Dialogs Add
 import DialogoAddTarjetaBlanca from 'StoreAdd/dialogAddTarjetaBlanca.vue'
+import DialogoAddProveedor from 'StoreAdd/dialogAddProveedor.vue'
 
 import notify_error from 'utils/notify/notify_error.js'
 
-export { STRINGS, DialogoAddTarjetaBlanca, notify_error }
+export { STRINGS, DialogoAddTarjetaBlanca, DialogoAddProveedor, notify_error }
