cecc90b5b79a64e43d6b07331347e20602cd8e17
cnunezjacas <cnunezjacas@gmail.com>
2025-08-05 11:28:43 -0400

    Importación de las rutas a los dialogos de Add para "Recepción de Tarjeta Blanca" y "Salida Tarjeta Producida"

--- src/utils/import_files_store.js
+++ src/utils/import_files_store.js
@@ -4,6 +4,8 @@ import { STRINGS } from 'utils/string.js'
 import DialogoAddTarjetaBlanca from 'StoreAdd/dialogAddTarjetaBlanca.vue'
 import DialogoAddProveedor from 'StoreAdd/dialogAddProveedor.vue'
 import DialogoAddTarjetaProducida from 'StoreAdd/dialogAddTarjetaProducida.vue'
+import DialogAddRecTarjetaBlanca from 'StoreAdd/dialogAddRecTarjetaBlanca.vue'
+import DialogAddSalidaTarjetaProducida from 'StoreAdd/dialogAddSalidaTarjetaProducida.vue'
 
 import notify_error from 'utils/notify/notify_error.js'
 
@@ -12,5 +14,7 @@ export {
   DialogoAddTarjetaBlanca,
   DialogoAddProveedor,
   DialogoAddTarjetaProducida,
+  DialogAddRecTarjetaBlanca,
+  DialogAddSalidaTarjetaProducida,
   notify_error,
 }
