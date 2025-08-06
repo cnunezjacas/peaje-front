d050b4870d306bb0a4c7475b24face9395857c39
cnunezjacas <cnunezjacas@gmail.com>
2025-08-06 09:56:47 -0400

    implementar función para verificar si un valor "key" existe en la API

---
+++ src/utils/utils_axios/nomencladores/checkCode.js
@@ -0,0 +1,22 @@
+// utils/verificarCodigo.js
+import api from 'src/axios.js'
+
+/**
+ * Función genérica para verificar si un valor ya existe en una API.
+ * @param {string} url - La URL de la API a consultar.
+ * @param {string} campo - El campo del objeto en response.data a verificar.
+ * @param {any} valor - El valor a buscar en ese campo.
+ * @returns {boolean} - true si existe, false si no.
+ */
+const verificarExistente = async (url, campo, valor) => {
+  try {
+    const response = await api.get(url)
+    console.log(response.data)
+    return response.data.some((element) => element[campo] === Number(valor))
+  } catch (error) {
+    console.error(error)
+    return false
+  }
+}
+
+export default verificarExistente
