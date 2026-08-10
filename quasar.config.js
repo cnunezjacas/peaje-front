abc3cd1ec2ee90ad2bd41fbb01212caa90d9fc7f
cnunezjacas <cnunezjacas@gmail.com>
2026-08-10 13:22:51 -0400

    (fix): eliminar boot/axios de la configuración de Quasar tras renombrar a api.js

--- quasar.config.js
+++ quasar.config.js
@@ -18,7 +18,7 @@ export default defineConfig((/* ctx */) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['axios', 'notify'],
+    boot: ['notify'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss', 'custom.css'],
