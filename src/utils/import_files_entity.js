f4cf19fb3de8890e5551d9780cd91a03744b6994
cnunezjacas <cnunezjacas@gmail.com>
2026-09-01 10:15:40 -0400

    refactor(entity): Actualizaciones en el fichero import_files_entity.js
    Cambios principales:
    - Archivo renombrado: dialog_add_estacion.vue -> AddEstacion.vue (PascalCase)
    - Archivo renombrado: dialog_edit_cuenta.vue -> UpdateCuenta.vue (PascalCase)
    - Archivo renombrado: table_cuenta.vue -> TableCuenta.vue (PascalCase)

--- src/utils/import_files_entity.js
+++ src/utils/import_files_entity.js
@@ -1,7 +1,7 @@
 //Dialogs Add
-import DialogoAddEstacion from 'EntityAdd/dialog_add_estacion.vue'
+import AddCuenta from 'src/pages/manage/entity/cuenta/add/AddCuenta.vue'
+import AddEstacion from 'src/pages/manage/entity/estacion/add/AddEstacion.vue'
 import DialogoAddAreaTrabajo from 'EntityAdd/dialog_add_area_trabajo.vue'
-import DialogoAddCuenta from 'EntityAdd/dialog_add_cuenta.vue'
 import DialogoAddTerminal from 'EntityAdd/dialog_add_terminal.vue'
 import DialogoAddPuntoDeVenta from 'EntityAdd/dialog_add_puntos_de_venta.vue'
 import DialogoAddOperador from 'EntityAdd/dialog_add_operador.vue'
@@ -9,31 +9,31 @@ import DialogoAddCliente from 'EntityAdd/dialog_add_cliente.vue'
 import DialogoAddTramitador from 'EntityAdd/dialog_add_tramitador.vue'
 
 //Dialogs Edit
-import DialogoEditEstacion from 'EntityEdit/dialog_edit_estacion.vue'
-import DialogoEditCuenta from 'EntityEdit/dialog_edit_cuenta.vue'
+import UpdateCuenta from 'src/pages/manage/entity/cuenta/update/UpdateCuenta.vue'
+import DialogoEditEstacion from 'src/pages/manage/entity/estacion/update/dialog_edit_estacion.vue'
 
 import DialogoDetailsEstacion from 'EntityDetails/details_estaciones.vue'
 //Dialogs Delete
-//import DialogDeleteGeneric from 'GenericDelete/dialog_delete_generic.vue'
+//import DialogDeleteGeneric from 'GenericDelete/DeleteGeneric.vue'
 
 //Tables
-import Table_Estacion from 'EntityTables/table_estacion.vue'
+import TableCuenta from 'src/pages/manage/entity/cuenta/table/TableCuenta.vue'
+import Table_Estacion from 'src/pages/manage/entity/estacion/table/table_estacion.vue'
 import TableADT from 'EntityTables/table_ADT.vue'
-import Table_Cuenta from 'EntityTables/table_cuenta.vue'
 
 export {
-  DialogoAddEstacion,
+  AddEstacion,
   DialogoAddAreaTrabajo,
-  DialogoAddCuenta,
+  AddCuenta,
   DialogoAddTerminal,
   DialogoAddPuntoDeVenta,
   DialogoAddOperador,
   DialogoAddCliente,
   DialogoAddTramitador,
   DialogoEditEstacion,
-  DialogoEditCuenta,
+  UpdateCuenta,
   DialogoDetailsEstacion,
   Table_Estacion,
   TableADT,
-  Table_Cuenta,
+  TableCuenta,
 }
